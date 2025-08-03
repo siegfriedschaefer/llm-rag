9f05a8c63fa739f18b55a110630b324ac2f9cfbe
SiggiS <siggis@schaefer.ai>
2025-08-03 17:47:16 +0200

    refactored

--- document_comparison/src/faiss_demo.py
+++ document_comparison/src/faiss_demo.py
@@ -1,21 +1,32 @@
 import numpy as np
+import faiss
 
 # FAISS index example for demonstration purposes
 # This code snippet creates a simple FAISS index with random data.
 
 
-d = 6                        # dimension
-nb = 50                      # database size
-nq = 5                       # number of queries
+vector_dim = 6                        # vector dimension
+data_set_size = 50                     # database size
+query_set_size = 5                       # number of queries
 
 np.random.seed(1234)
-xb = np.random.random((nb, d)).astype('float32')
 
-xb[:, 0] += np.arange(nb) / 1000.
-print(f"xb.shape: {xb.shape}")
-print(f"{xb[:10]}")
+# Generate random vectors for the database and queries
+data_set = np.random.random((data_set_size, vector_dim)).astype('float32')
 
-xq = np.random.random((nq, d)).astype('float32')
-xq[:, 0] += np.arange(nq) / 1000.
-print(f"xq.shape: {xq.shape}")
-print(f"{xq[:10]}")
+data_set[:, 0] += np.arange(data_set_size) / 1000.
+print(f"dataset.shape: {data_set.shape}")
+print(f"{data_set[:10]}")
+
+query_set = np.random.random((query_set_size, vector_dim)).astype('float32')
+query_set[:, 0] += np.arange(query_set_size) / 1000.
+print(f"query_set.shape: {query_set.shape}")
+print(f"{query_set[:10]}")
+
+ # initialize the index
+index = faiss.IndexFlatL2(vector_dim)  
+print(index.is_trained)
+
+# add vectors to the index
+index.add(data_set)
+print(index.ntotal)
